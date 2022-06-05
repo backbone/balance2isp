1c069d5b38643b4f9f7b8c5e910ccd2df4a9a435
backbone <backbone@backbone.ws>
2022-06-05 16:32:36 +0300

    test.sh: run from any dir

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-for url in `cat sites.url`; do tracepath -m1 -n $url; done | grep 192.168
+EXE_PATH="`readlink -f $0`"
+PRJ_PATH="${EXE_PATH%/*}"
+for url in `cat $PRJ_PATH/sites.url`; do tracepath -m1 -n $url; done | grep 192.168
